5eaa96df6cc8984302e6ed764d91cbedc17ed602
NirjalaGhimire <nirjalaghimire012@gmail.com>
2025-11-12 12:11:45 +0545

    first commit

---
+++ Task1/Operatorscs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Operatorscs
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("sum: " + (a+b));
+        }
+
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Difference: " + (a - b));
+        }
+
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine("multiply: " + (a * b));
+        }
+
+        public void Divide(int a , int b)
+        {
+            if (b != 0)
+                Console.WriteLine("Division: " + (a / b));
+            else
+                Console.WriteLine("Error : Division by zero is not allowed");
+        }
+
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operatorscs op = new Operatorscs();
+
+            op.Add(5,6);
+            op.Subtract(3, 2);
+            op.Multiply(5,6);
+            op.Divide(6,2);
+            op.OddEvenFinder(5);
+            op.OddEvenFinder(56);
+        }
+    }
+}
