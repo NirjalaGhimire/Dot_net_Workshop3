c0b01f604571459ff7037332bc444f7534092802
NirjalaGhimire <nirjalaghimire012@gmail.com>
2025-11-13 08:39:00 +0545

    completed and updated Task2 Nulloperations

---
+++ Task2/NullOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class NullOperations
+    {
+        public void PerformNullChecks()
+        {
+            // Declare variable as null
+            string username = null;
+
+            // Check using ternary operator
+            string ternaryResult = (username == null) ? "Username is not available" : username;
+            Console.WriteLine("Ternary Operator Result: " + ternaryResult);
+
+            // Check using null-coalescing operator
+            string coalescingResult = username ?? "Username is not available";
+            Console.WriteLine("Null-Coalescing Operator Result: " + coalescingResult);
+
+            // Assign value using null-coalescing assignment
+            username ??= "DefaultUser";
+            Console.WriteLine("After Null-Coalescing Assignment, username = " + username);
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create object and call method
+            NullOperations nullOps = new NullOperations();
+            nullOps.PerformNullChecks();
         }
     }
 }
