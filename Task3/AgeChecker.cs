bbd10cf20a860277c35245ecf82e37be6e256528
NirjalaGhimire <nirjalaghimire012@gmail.com>
2025-11-13 09:03:02 +0545

    completed Task 3

---
+++ Task3/AgeChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class AgeChecker
+    {
+        public void CheckAge()
+        {
+            // Ask user to enter age
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Check age using only if-else
+            if (age < 13)
+            {
+                Console.WriteLine("You are a Child.");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("You are a Teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are an Adult.");
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeChecker checker = new AgeChecker();
+            checker.CheckAge();
+        }
+    }
+}
